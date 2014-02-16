ca6bcfe9876ff715aceb7229d028d5efae5f8166
Lukáš Havrlant <lukas.havrlant@gmail.com>
2014-02-16 17:00:29 +0100

    Initial Commit

---
+++ parkc/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  parkc
+//
+//  Created by Lukáš Havrlant on 16.2.14.
+//  Copyright (c) 2014 Lukáš Havrlant. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
