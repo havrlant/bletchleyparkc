9b0f4367b528b86c41d4b508a6efb75fa4c80335
Lukáš Havrlant <lukas.havrlant@gmail.com>
2014-02-24 20:29:07 +0100

    refactorng

--- parkc/tests/ciphers_test.h
+++ parkc/tests/ciphers_test.h
@@ -1,7 +1,6 @@
 #ifndef parkc_ciphers_test_h
 #define parkc_ciphers_test_h
 
-#include <string.h>
 #include "../lib/unittest.h"
 #include "../ciphers/caesar.h"
 #include "../ciphers/vigenere.h"
---
+++ parkc/tests/utilities_tests.h
@@ -0,0 +1,6 @@
+#ifndef parkc_utilities_tests_h
+#define parkc_utilities_tests_h
+
+#include "../lib/unittest.h"
+
+#endif
