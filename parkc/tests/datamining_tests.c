7155a43e8bbbc2afce1b6db23722968da1193e2e
Lukáš Havrlant <lukas.havrlant@gmail.com>
2014-02-24 21:45:35 +0100

    refactoring

--- parkc/tests/datamining_tests.c
+++ parkc/tests/datamining_tests.c
@@ -1,25 +1,18 @@
 #include "datamining_tests.h"
 
 static void test_ngrams_ocur() {
-    int expected[26] = {3, 2, 1};
-    int expected2[26] = {0};
-    int expected3[26*26] = {2, 1};
-    int expected4[26*26] = {3};
-    
-    assert_array_eq(expected, ngrams_ocur("aaabbc", 1), ALPHABET_LENGTH, int);
-    assert_array_eq(expected2, ngrams_ocur("", 1), ALPHABET_LENGTH, int);
-    assert_array_eq(expected3, ngrams_ocur("aaab", 2), ALPHABET_LENGTH * ALPHABET_LENGTH, int);
-    assert_array_eq(expected4, ngrams_ocur("aaaa", 2), ALPHABET_LENGTH * ALPHABET_LENGTH, int);
+    int length = ALPHABET_LENGTH;
+    assert_array_eq(((int[]){3, 2, 1}), ngrams_ocur("aaabbc", 1), length, int);
+    assert_array_eq(((int[]){0}), ngrams_ocur("", 1), length, int);
+    assert_array_eq(((int[]){2, 1}), ngrams_ocur("aaab", 2), length * length, int);
+    assert_array_eq(((int[]){3}), ngrams_ocur("aaaa", 2), length * length, int);
 }
 
 static void test_ngrams_freq() {
-    double expected[26] = {30, 20, 50};
-    double expected2[26] = {0, 100};
-    double expected3[26 * 26] = {75, 25};
-    
-    assert_array_eq(expected, ngrams_freq("aaabbccccc", 1), ALPHABET_LENGTH, double);
-    assert_array_eq(expected2, ngrams_freq("bbbbbbbbbb", 1), ALPHABET_LENGTH, double);
-    assert_array_eq(expected3, ngrams_freq("aaab", 1), ALPHABET_LENGTH, double);
+    int length = ALPHABET_LENGTH;
+    assert_array_eq(((double[]){30, 20, 50}), ngrams_freq("aaabbccccc", 1), length, double);
+    assert_array_eq(((double[]){0, 100}), ngrams_freq("bbbbbbbbbb", 1), length, double);
+    assert_array_eq(((double[]){75, 25}), ngrams_freq("aaab", 1), length, double);
 }
 
 static void test_word_ocurences() {
@@ -30,8 +23,16 @@ static void test_word_ocurences() {
     assert_eq(0, words_occurences(sarray, "Scala is also pretty damn good."));
 }
 
+static void test_deviation() {
+    assert_eq(2, deviation((double[]){1, 2, 3}, (double[]){2, 3, 3}, 3));
+    assert_eq(6, deviation((double[]){1, 1, 2, 4}, (double[]){0, 3, 2, 5}, 4));
+    assert_eq(0, deviation((double[]){1, 2, 3, 4}, (double[]){1, 2, 3, 4}, 4));
+    assert_eq(0.25, deviation((double[]){1, 1.5}, (double[]){1, 1}, 2));
+}
+
 void all_datamining_tests() {
     test_ngrams_ocur();
     test_ngrams_freq();
     test_word_ocurences();
+    test_deviation();
 }
